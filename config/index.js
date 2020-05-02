a2db4ca29ba46d33baa87ab6491c271c4d08c296
Vaibhav Raj Singh <vaibhavrajsingh2001@gmail.com>
2020-05-02 19:13:59 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var configValues=require('./config');
 
 module.exports={
     getDbConnnectionString: function(){
-       return 'mongodb+srv://test:lordbuddha@nodetodo-sample-xhpu0.mongodb.net/test?retryWrites=true&w=majority';
+       return 'mongodb+srv://test:@nodetodo-sample-xhpu0.mongodb.net/test?retryWrites=true&w=majority';
     }
 }
